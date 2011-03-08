826a06ffba4061b694099befe4cc338af87ad487
Daniel Upton <daniel@leopard-solutions.com>
2011-03-08 09:59:30 +0000

    Initial version of the plugin (1 small known bug with multiple slideshows)

---
+++ js/jquery.slideshow.js
@@ -0,0 +1,56 @@
+// Simple slideshow plugin - Leopard Business Solutions
+// Daniel Upton [daniel@ileopard.co]
+(function($){ 
+	$.fn.extend({ 
+		slideshow: function(options) { 
+			var defaults = { 
+				time: 3000 
+			};		 
+			
+			var options  = $.extend(defaults, options); 
+		
+			return this.each(function() {
+				var obj = $(this); 
+
+				// Get the first image and add mark it as current 
+				var firstImage = $(obj).children('.images').children(':first-child').addClass('current');
+				var firstImageUrl = firstImage.attr('src');
+		
+				// Add the first slide and set the first image as the slide's background
+				$(obj).append($('<div class="slide currentSlide">').css('background-image', 'url(' + firstImageUrl + ')'));
+		
+				// Swap the slides (pretty self explanitory i know)
+				function switchSlides(){
+					var currentImage = $(obj).children('.images').children('img.current');
+		  			
+					// If the next child exists use it, else get the first child again
+					var nextImage = currentImage.next().length ? currentImage.next() : firstImage;
+					var nextImageUrl = nextImage.attr('src');
+					
+					// Get the current slide
+					var currentSlide = $(obj).children('.currentSlide');
+					
+					// Add the next slide
+					nextSlide = $('<div class="slide nextSlide">').css('background-image', 'url(' + nextImageUrl + ')');
+					$(obj).append(nextSlide);
+		 			
+					// Fade next image in and when its done can the old one
+					nextSlide.fadeIn(function(){
+					
+						// Remove the old slide
+						currentSlide.remove();
+		  			
+						// Mark the new slide as current
+						nextSlide.removeClass('nextSlide');
+						nextSlide.addClass('currentSlide');
+					});
+		
+					// Swap who's current the current image
+					nextImage.addClass('current');
+					currentImage.removeClass('current');
+				}
+				// Call swap slides every 3 seconds
+				setInterval( function() { switchSlides(); }, options.time );
+			})}
+		});
+})(jQuery);
